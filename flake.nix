59af66549bdd19dbe74318b73236412979dbf638
llamato <tguessbacher@gmx.de>
2026-08-13 17:17:13 +0200

    Added demo package to flake

--- flake.nix
+++ flake.nix
@@ -9,11 +9,51 @@
         "x86_64-darwin"
         "aarch64-darwin"
         "armv7l-linux"
+        "riscv64-linux"
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs systems;
       pkgsFor = system: import nixpkgs { inherit system; };
     in {
+      packages = forAllSystems (system:
+        let pkgs = pkgsFor system; in {
+          demo = pkgs.gcc14Stdenv.mkDerivation {
+            pname = "cbmtext";
+            version = "0.1.0";
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = with pkgs; [
+              gnumake
+              keepBuildTree
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              (glfw.overrideAttrs { cmakeFlags = [
+                (lib.cmakeBool "GLFW_BUILD_WAYLAND" false)
+                (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+              ];})
+              libGL
+              libGLU
+              mesa
+              libglvnd
+              libX11
+              libc
+            ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/demo $out/bin/cbmtextdemo
+            '';
+            
+            meta = with pkgs.lib; {
+              description = "An OpenGL native space trading game";
+              homepage = "http://www.github.com/Llamato/Spacer3000";
+              mainProgram = "spacer3000";
+              platforms = platforms.unix;
+            };
+          };
+        }
+      );
       devShells = forAllSystems (system:
         let 
           pkgs = pkgsFor system;
